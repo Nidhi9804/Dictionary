15494aa6f63d842884c954f00d5157e61a90dcdd
Nidhi <nidhi9804@gmail.com>
2023-03-18 15:10:29 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,79 +5,84 @@ import { FaSearch } from "react-icons/fa";
 import { FcSpeaker } from "react-icons/fc";
 
 function App() {
-// Setting up the initial states using react hook 'useState'
+	// Setting up the initial states using react hook 'useState'
 
-const [data, setData] = useState("");
-const [searchWord, setSearchWord] = useState("");
+	const [data, setData] = useState("");
+	const [searchWord, setSearchWord] = useState("");
 
-// Function to fetch information on button
-// click, and set the data accordingly
-function getMeaning() {
-	Axios.get(
-	`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
-	).then((response) => {
-	setData(response.data[0]);
-	});
-}
+	// Function to fetch information on button
+	// click, and set the data accordingly
+	function getMeaning() {
+		Axios.get(
+			`https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
+		).then((response) => {
+			setData(response.data[0]);
+		});
+	}
 
-// Function to play and listen the
-// phonetics of the searched word
-function playAudio() {
-	let audio = new Audio(data.phonetics[0].audio);
-	audio.play();
-}
+	// Function to play and listen the
+	// phonetics of the searched word
+	function playAudio() {
+		let audio = new Audio(data.phonetics[0].audio);
+		audio.play();
+	}
 
-return (
-	<div className="App">
-	<h1>Glossary</h1>
-	<div className="searchBox">
+	return (
+		<div className="App">
 
-		{/* // Taking user input */}
-		<input
-		type="text"
-		placeholder="Search..."
-		onChange={(e) => {
-			setSearchWord(e.target.value);
-		}}
-		/>
-		<button
-		onClick={() => {
-			getMeaning();
-		}}
-		>
-		<FaSearch size="20px" />
-		</button>
-	</div>
-	{data && (
-		<div className="showResults">
-		<h2>
-			{data.word}{" "}
-			<button
-			onClick={() => {
-				playAudio();
-			}}
-			>
-			<FcSpeaker size="26px" />
-			</button>
-		</h2>
-		<h4>Parts of speech:</h4>
+		<div className="box">
 
-		
-<p>{data.meanings[0].partOfSpeech}</p>
+		<h1>Glossary</h1>
+			<div className="searchBox">
 
-
-		<h4>Definition:</h4>
-
-		
-<p>{data.meanings[0].definitions[0].definition}</p>
+				{/* // Taking user input */}
+				<input
+					type="text"
+					placeholder="Search..."
+					onChange={(e) => {
+						setSearchWord(e.target.value);
+					}}
+				/>
+				<button
+					onClick={() => {
+						getMeaning();
+					}}
+				>
+					<FaSearch size="20px" />
+				</button>
+			</div>
+			{data && (
+				<div className="showResults">
+					<h2>
+						{data.word}{" "}
+						<button
+							onClick={() => {
+								playAudio();
+							}}
+						>
+							<FcSpeaker size="26px" />
+						</button>
+					</h2>
+					<h4>Parts of speech:</h4>
 
 
-		
+					<p>{data.meanings[0].partOfSpeech}</p>
+
+
+					<h4>Definition:</h4>
+
+
+					<p>{data.meanings[0].definitions[0].definition}</p>
+
+
+
+
+				</div>
+			)}
 
 		</div>
-	)}
-	</div>
-);
+		</div>
+	);
 }
 
 export default App;
